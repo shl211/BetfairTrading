7b858358e04035504107f40b267102f6dbf66deb
StephenLiu_WSL <stephen.liu21@outlook.com>
2025-06-13 22:04:21 +0100

    Edit constructor for better performance

--- BetfairAPI/betting_type/event_type_results.cpp
+++ BetfairAPI/betting_type/event_type_results.cpp
@@ -2,7 +2,7 @@
 
 namespace BetfairAPI::BettingType {
     EventTypeResults::EventTypeResults(EventType event_type, int market_count)
-        : event_type_(event_type), market_count_(market_count) {};
+        : event_type_(std::move(event_type)), market_count_(market_count) {};
 
     int EventTypeResults::getMarketCount() const {
         return market_count_;
